eb678a0b040c0045bb1c69cfbad5d29482bdc19e
andrisole92 <andrisole92@interstruct.com>
2018-04-11 16:03:59 +0100

    Local Stream

---
+++ webrtc.js
@@ -0,0 +1,36 @@
+navigator.getUserMedia = navigator.getUserMedia || navigator.mozGetUserMedia || navigator.webkitGetUserMedia;
+window.RTCPeerConnection = window.RTCPeerConnection || window.mozRTCPeerConnection || window.webkitRTCPeerConnection;
+window.RTCIceCandidate = window.RTCIceCandidate || window.mozRTCIceCandidate || window.webkitRTCIceCandidate;
+window.RTCSessionDescription = window.RTCSessionDescription || window.mozRTCSessionDescription || window.webkitRTCSessionDescription;
+
+
+var localVideo;
+var remoteVideo;
+var peerConnection;
+var peerConnectionConfig = {'iceServers': [{'url': 'stun:stun.services.mozilla.com'}, {'url': 'stun:stun.l.google.com:19302'}]};
+
+function pageReady() {
+    localVideo = document.getElementById('localVideo');
+    remoteVideo = document.getElementById('remoteVideo');
+
+
+    var constraints = {
+        video: true,
+        audio: true,
+    };
+
+    if(navigator.getUserMedia) {
+        navigator.getUserMedia(constraints, getUserMediaSuccess, getUserMediaError);
+    } else {
+        alert('Your browser does not support getUserMedia API');
+    }
+}
+
+function getUserMediaSuccess(stream) {
+    localStream = stream;
+    localVideo.src = window.URL.createObjectURL(stream);
+}
+
+function getUserMediaError(error) {
+    console.log(error);
+}
